2b8af4d9b14dcfc2372960ba25bb17a2da2c4731
Christine-horrocks <splogy2001@hotmail.com>
2018-06-27 16:02:21 +0100

    Initial Commit

---
+++ Guess Who House of Lords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guess Who House of Lords
+//
+//  Created by HORROCKS, Christine on 27/06/2018.
+//  Copyright © 2018 HORROCKS, Christine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Guess Who House of LordsTests/Guess_Who_House_of_LordsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guess_Who_House_of_LordsTests.swift
+//  Guess Who House of LordsTests
+//
+//  Created by HORROCKS, Christine on 27/06/2018.
+//  Copyright © 2018 HORROCKS, Christine. All rights reserved.
+//
+
+import XCTest
+@testable import Guess_Who_House_of_Lords
+
+class Guess_Who_House_of_LordsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
